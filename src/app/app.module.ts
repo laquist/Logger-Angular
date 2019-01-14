b1dedcb6a8338465b28bfba339c2f1665218cc8e
Lars Lynnerup <lars9865@edu.campusvejle.dk>
2019-01-14 13:08:40 +0100

    Importeret fullcalendar, ng-fullcalendar og jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { MessagesComponent } from './messages/messages.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { NavComponent } from './nav/nav.component';
 
+import * as $ from 'jquery';
+import { FullCalendarModule } from 'ng-fullcalendar';
+import { CalendarComponent } from './calendar/calendar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MessagesComponent,
     TasksComponent,
-    NavComponent
+    NavComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $('#calendar').fullCalendar({
+      header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,basicWeek,basicDay'
+      },
+      defaultDate: '2016-12-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+          {
+              title: 'All Day Event',
+              start: '2016-12-01'
+          },
+          {
+              title: 'Long Event',
+              start: '2016-12-07',
+              end: '2016-12-10'
+          },
+          {
+              id: 999,
+              title: 'Repeating Event',
+              start: '2016-12-09T16:00:00'
+          },
+          {
+              id: 999,
+              title: 'Repeating Event',
+              start: '2016-12-16T16:00:00'
+          },
+          {
+              title: 'Conference',
+              start: '2016-12-11',
+              end: '2016-12-13'
+          },
+          {
+              title: 'Meeting',
+              start: '2016-12-12T10:30:00',
+              end: '2016-12-12T12:30:00'
+          },
+          {
+              title: 'Lunch',
+              start: '2016-12-12T12:00:00'
+          },
+          {
+              title: 'Meeting',
+              start: '2016-12-12T14:30:00'
+          },
+          {
+              title: 'Happy Hour',
+              start: '2016-12-12T17:30:00'
+          },
+          {
+              title: 'Dinner',
+              start: '2016-12-12T20:00:00'
+          },
+          {
+              title: 'Birthday Party',
+              start: '2016-12-13T07:00:00'
+          },
+          {
+              title: 'Click for Google',
+              url: 'https://google.com/',
+              start: '2016-12-28'
+          }
+      ]
+    });
+
+    console.log('ngOnInit loaded   (calendar.component.ts)');
+  }
+
+}
